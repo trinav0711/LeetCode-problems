51ff5868733d083560316cf9a37813114662dbd0
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-02-14 19:21:02 -0500

    Time: 0 ms (100%), Space: 10.8 MB (21.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,13 +6,9 @@ public:
         dp.push_back(nums[0]);
         if(nums.size()<=1)
             return dp[0];
-        dp.push_back(nums[1]);
-        for(int i=2;i<nums.size();i++) {
-            int m=dp[i-1];
-            for(int j=i-2;j>=0;j--)
-                m=max(m, dp[j]+nums[i]);
-            dp.push_back(m);
-        }
-        return nums.size()>2?dp[nums.size()-1]:max(dp[0], dp[1]);
+        dp.push_back(max(nums[1], nums[0]));
+        for(int i=2;i<nums.size();i++) 
+            dp.push_back(max(dp[i-1], nums[i]+dp[i-2]));
+        return dp[nums.size()-1];
     }
 };
