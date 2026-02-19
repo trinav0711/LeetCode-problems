a6c55a28690f973e0c32a5218027dc9857b6b5d2
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-02-18 22:22:12 -0500

    Time: 0 ms (100%), Space: 13.1 MB (98.09%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int findRow(vector<vector<int>>& m, int t, int s, int e) {
+        if(t==m[s][0])
+            return s;
+        if(t>=m[e][0])
+            return e;
+        if(s>=e)
+            return s;
+        int mid=(s+e)/2;
+        if(t==m[mid][0])
+            return mid;
+        cout<<"start="<<s<<" and end="<<e<<"\n";
+        if(t>m[mid][0])
+            return findRow(m, t, mid, e-1);
+        return findRow(m, t, s, mid-1);
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(target<matrix[0][0] || target>matrix[matrix.size()-1][matrix[0].size()-1])
+            return false;
+        auto& row=matrix[findRow(matrix, target, 0, matrix.size()-1)];
+        auto itr=lower_bound(row.begin(), row.end(), target);
+        if(itr==row.end())
+            return false;
+        return *itr==target;
+    }
+};
