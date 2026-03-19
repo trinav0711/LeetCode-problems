a442a45ac238380cc939fd1d436648e6c6b94655
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-03-19 00:49:00 -0400

    Time: 0 ms (100%), Space: 10 MB (97.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,13 @@
 class Solution {
-    vector<int> dp;
 public:
     int rob(vector<int>& nums) {
-        dp.reserve(nums.size());
+        vector<int> dp;
         dp.push_back(nums[0]);
-        if(nums.size()<=1)
-            return dp[0];
-        dp.push_back(max(nums[1], nums[0]));
-        for(int i=2;i<nums.size();i++) 
-            dp.push_back(max(dp[i-1], nums[i]+dp[i-2]));
-        return dp[nums.size()-1];
+        if(nums.size()<2)
+            return dp.back();
+        dp.push_back(max(nums[0], nums[1]));
+        for(int i=2;i<nums.size();i++)
+            dp.push_back(max(dp[i-2]+nums[i], dp[i-1]));
+        return dp.back();
     }
 };
