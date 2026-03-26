1dc44d7d986dc1620f060d6078756ad5468b2050
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-03-26 00:32:24 -0400

    Time: 0 ms (100%), Space: 14.9 MB (62.84%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,23 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-private:
-    set<int> m;
-    vector<int> ans;
-    void traverse(TreeNode *node, int l) {
-        if(!node)
-            return;
-        if(m.find(l)==m.end()){
-            m.insert(l);
-            ans.push_back(node->val);
-        }
-        traverse(node->right, l+1);
-        traverse(node->left, l+1);
-    }
+class Solution {   
 public:
     vector<int> rightSideView(TreeNode* root) {
-        traverse(root, 1);
+        if(!root)
+            return {};
+        queue<TreeNode*> q;
+        vector<int> ans;
+        q.push(root);
+        while(!q.empty()) {
+            int s=q.size();
+            ans.push_back(q.front()->val);
+            while(s--) {
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                q.pop();
+            }
+        }
         return ans;
     }
 };
