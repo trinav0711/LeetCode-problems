50503bb1cbad8eb358f8b08d9613220e886fb2ee
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-04-20 01:20:59 -0400

    Time: 5 ms (72.13%), Space: 34.9 MB (96.27%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
-    int BFS(vector<vector<char>>& maze, pair<int, int>start){
-        set<pair<int, int>>visited;
-        queue<pair<int, int>>q;
-        pair<int, int> node = start;
-        map<pair<int, int>, int>ans;
-        ans[node] = 0;
-        q.push(node);
-        visited.insert(node);
-        while(!q.empty()){
-            node = q.front();
-            q.pop();
-            if(node != start && (node.first == 0 || node.first == maze.size()-1 || node.second == maze[0].size() -1 || node.second == 0)){
-                return ans[node];
-            }
-
-            int i=node.first;
-            int j=node.second;
-
-            if((i-1)>=0 && maze[i-1][j] == '.' && visited.find({i-1,j}) == visited.end()){
-                visited.insert({i-1,j});
-                ans[{i-1,j}] = ans[node]+1;
-                q.push({i-1,j});
-            }
-            if((i+1)<=maze.size()-1 && maze[i+1][j] == '.' && visited.find({i+1,j}) == visited.end()){
-                visited.insert({i+1,j});
-                ans[{i+1,j}] = ans[node]+1;
-                q.push({i+1,j});
-            }
-            if((j+1)<=maze[0].size()-1 && maze[i][j+1] == '.' && visited.find({i,j+1}) == visited.end()){
-                visited.insert({i,j+1});
-                ans[{i,j+1}] = ans[node]+1;
-                q.push({i,j+1});
-            }
-            if((j-1)>=0 && maze[i][j-1] == '.' && visited.find({i,j-1}) == visited.end()){
-                visited.insert({i,j-1});
-                ans[{i,j-1}] = ans[node]+1;
-                q.push({i,j-1});
-            }
-
-
-        }
-
-        return -1;
-
-    }
-
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        pair<int,int>start = {entrance[0], entrance[1]};
-        return BFS(maze, start);
+        int n=maze.size(), m=maze[0].size();
+        queue<pair<int,int>> q;
+        int ctr=0;
+        q.push({entrance[0], entrance[1]});
+        maze[entrance[0]][entrance[1]]='+';
+        while(!q.empty()) {
+            int s=q.size();
+            while(s--) {
+                auto& [i,j] = q.front();
+                if((!i || i==n-1 || !j || j==m-1) && ctr) {
+                    return ctr;
+                }
+                if(i && maze[i-1][j]!='+') {
+                    q.push({i-1, j});
+                    maze[i-1][j]='+';
+                }
+                if(i<n-1 && maze[i+1][j]!='+') {
+                    maze[i+1][j]='+';
+                    q.push({i+1,j});
+                }
+                if(j && maze[i][j-1]!='+') {
+                    maze[i][j-1]='+';
+                    q.push({i,j-1});
+                }
+                if(j<m-1 && maze[i][j+1]!='+') {
+                    maze[i][j+1]='+';
+                    q.push({i,j+1});
+                }
+                q.pop();
+            }
+            ++ctr;
+        }
+        return -1;
     }
 };
