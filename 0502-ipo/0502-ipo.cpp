3116ec6a7f60dff6bd5e26456d4ea705fbf83e35
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-05-09 02:45:46 -0400

    Time: 72 ms (68.11%), Space: 131.5 MB (29.33%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<profits.size();++i) {
+            v.push_back({capital[i], profits[i]});
+        }
+        sort(v.begin(), v.end());
+        priority_queue<int> pq;
+        int i=0;
+        while(k--) {
+            while(i<v.size() && v[i].first<=w) {
+                pq.push(v[i].second);
+                ++i;
+            }
+            if(pq.empty())
+                break;
+            w+=(pq.top());
+            pq.pop();
+        }
+        return w;
+    }
+};
