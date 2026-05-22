dda839304d7f6161cc1a7e4e2a1b879b0a124d7c
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-05-22 00:46:35 -0400

    Time: 0 ms (100%), Space: 7.7 MB (57.78%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans="";
+        while(columnNumber--) {
+            ans=ans+char((columnNumber%26)+'A');
+            columnNumber/=26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
