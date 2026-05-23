f907daeabdb0a5290dd4bd08ba3241aec946876f
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-05-23 02:16:58 -0400

    Time: 0 ms (100%), Space: 7.9 MB (11.43%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        while(n%4==0) {
+            n/=4;
+        }
+        return n==1;
+    }
+};
