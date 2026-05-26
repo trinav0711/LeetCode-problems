1f8d91f77b025da29fd97a446b0d992d5e7bf4a6
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-05-26 02:47:09 -0400

    Time: 375 ms (6.65%), Space: 211.2 MB (5.04%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,25 +1,32 @@
 class Solution {
 private:
-    int dfs(int src, vector<bool>& v, unordered_map<int, set<int>>& m) {
-        int ans=0;
-        v[src]=1;
-        for(int to:m[src]) {
-            if(!v[abs(to)]) {
-                ans+=(dfs(abs(to), v, m));
-                if(to>0)
+    int ans;
+    unordered_set<int> nodes;
+    unordered_map<int, vector<int>> orig;
+    void dfs(unordered_map<int,vector<int>>& m, unordered_set<int>& visit, int cur) {
+        visit.insert(cur);
+        if(visit.size()<nodes.size()) {
+            for(int x:m[cur])
+                if(visit.find(x)==visit.end())
+                    dfs(m, visit, x);
+            for(int x:orig[cur]) {
+                if(visit.find(x)==visit.end()) {
                     ++ans;
+                    dfs(m, visit, x);
+                }
             }
         }
-        return ans;
     }
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-        unordered_map<int, set<int>> m;
-        vector<bool> visit(n, 0);
+        unordered_map<int, vector<int>> m; unordered_set<int> visit;
         for(auto& v:connections) {
-            m[v[0]].insert(v[1]);
-            m[v[1]].insert(-v[0]);
+            nodes.insert(v[0]); nodes.insert(v[1]);
+            m[v[1]].push_back(v[0]);
+            orig[v[0]].push_back(v[1]);
         }
-        return dfs(0, visit, m);
+        dfs(m, visit, 0);
+        if(visit.size()!=nodes.size()) return -1;
+        return ans;
     }
 };
