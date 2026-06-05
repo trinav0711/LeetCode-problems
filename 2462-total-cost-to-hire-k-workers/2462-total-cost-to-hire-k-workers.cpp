34526a844ed160a8d78dd64ab9f3bce697efa15f
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-06-05 01:31:45 -0400

    Time: 2282 ms (5%), Space: 80 MB (19.05%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        int n=costs.size();
-        int i=0, j=n-1;
-        priority_queue<int, vector<int>, greater<int>> f,l;
-        for(;i<candidates;++i)
-            f.push(costs[i]);
-        while(j>=i && j>=n-candidates)
-            l.push(costs[j--]);
+        int first=candidates-1, end=costs.size()-candidates;
         long long ans=0;
+        struct Str {
+            Str() {}
+            bool operator()(pair<int,int>&x, pair<int,int>&y) {
+                if(x.first==y.first) return x.second>y.second;
+                return x.first>y.first;
+            }
+        };
+        priority_queue<pair<int,int>, vector<pair<int,int>>, Str> pq1, pq2;
+        for(int i=0;i<=first;++i) pq1.push({costs[i], i});
+        for(int i=max(first+1, end);i<costs.size();++i) pq2.push({costs[i], i});
+        cout<<pq1.size()<< " "<<pq2.size()<<endl;
         while(k--) {
-            int ans1=(f.empty())?INT_MAX:f.top();
-            int ans2=(l.empty())?INT_MAX:l.top();
-            if(ans1<=ans2) {
-                ans+=f.top();
-                f.pop();
-                if(i<=j)
-                    f.push(costs[i++]);
+            if(pq2.empty() || (!pq1.empty() && pq1.top().first<=pq2.top().first)) {
+                ans+=(pq1.top().first);
+                cout<<"chose first "<<pq1.top().first<<" "<<pq1.top().second<<endl;
+                pq1.pop();
+                if(first+1<end) {
+                    cout<<"added first"<<endl;
+                    pq1.push({costs[first+1], first+1});
+                    first++;
+                }
             }
             else {
-                ans+=l.top();
-                l.pop();
-                if(j>=i)
-                    l.push(costs[j--]);
+                ans+=(pq2.top().first);
+                cout<<"chose second "<<pq2.top().first<<" "<<pq2.top().second<<endl;
+                pq2.pop();
+                if(end-1>first) {
+                    cout<<"added second\n";
+                    pq2.push({costs[end-1], end-1});
+                    --end;
+                }
             }
         }
         return ans;
