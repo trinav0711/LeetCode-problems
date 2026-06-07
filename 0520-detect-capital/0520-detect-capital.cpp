c3f10597cb461d8fa7675b7c97091b4b67ec2f85
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-06-07 02:13:50 -0400

    Time: 0 ms (100%), Space: 8.1 MB (50.74%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.length()==1) return true;
+        if(word[0]>='A' && word[0]<='Z') {
+            bool lower=(word[1]>='a' && word[1]<='z');
+            for(int i=2;i<word.length();++i) {
+                if(lower && (word[i]<'a' || word[i]>'z')) return false;
+                else if(!lower && (word[i]<'A' || word[i]>'Z')) return false;
+            }
+        }
+        else {
+            for(int i=1;i<word.length();++i) {
+                if(word[i]<'a' || word[i]>'z') return false;
+            }
+        }
+        return true;
+    }
+};
