faeef2ad1acfa99fe535d917b2aff21f05d3aeb2
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-08-18 13:17:42 -0400

    Time: 78 ms (49.82%), Space: 148.6 MB (64.49%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -3,25 +3,25 @@ private:
     priority_queue<int> left;
     priority_queue<int, vector<int>, greater<int>> right;
 public:
-    MedianFinder(): left{}, right{} {}
+    MedianFinder() {}
     
     void addNum(int num) {
-        if(left.empty() || num<left.top()) left.push(num);
-        else right.push(num);
+        left.push(num);
+        if(!right.empty() && left.top()>right.top()) {
+            right.push(left.top());
+            left.pop();
+            left.push(right.top());
+            right.pop();
+        }
         if(left.size()>right.size()+1) {
             right.push(left.top());
             left.pop();
         }
-        else if(right.size()>left.size()) {
-            left.push(right.top());
-            right.pop();
-        }
     }
     
     double findMedian() {
-        if(left.size()==right.size())
-            return (static_cast<double>(left.top())+static_cast<double>(right.top()))/2;
-        return static_cast<double>(left.top());
+        if(left.size()>right.size()) return static_cast<double>(left.top());
+        return static_cast<double>(((double)(left.top())+(double)(right.top()))/2);
     }
 };
 
