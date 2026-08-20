8400939d8d11dca389bcf7c93f408596ccea47e8
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-08-20 13:34:41 -0400

    Time: 0 ms (100%), Space: 20.7 MB (54.35%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int> jump(nums.size());
-        jump[nums.size()-1]=0;
-        for(int i=nums.size()-2;i>=0;i--){
-            if(nums[i]==0)
-                jump[i]=1001;
-            else{
-                int minEle=jump[i+1];
-                for(int j=i+2;j<=i+nums[i] && j<nums.size();j++)
-                    minEle=min(minEle,jump[j]);
-                jump[i]=1+minEle;
+        int reach=0, end=0, jumps=0;
+        for(int i=0;i<nums.size();++i) {
+            if(i==nums.size()-1) return jumps;
+            reach=max(reach, i+nums[i]);
+            if(i==end) {
+                ++jumps;
+                end=reach;
             }
         }
-        return jump[0];
+        return jumps;
     }
 };
