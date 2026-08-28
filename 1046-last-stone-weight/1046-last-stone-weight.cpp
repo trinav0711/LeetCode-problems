e5bb6e74f8c28257e6dac24a3ab37b10ec29f63c
Trinav Bhattacharyya <trinav0711@gmail.com>
2026-08-28 17:05:02 -0400

    Time: 1 ms (7.14%), Space: 10.1 MB (12.52%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        int ans=0;
         priority_queue<int> pq;
         for(int x:stones) pq.push(x);
         while(pq.size()>1) {
@@ -11,7 +10,7 @@ public:
             pq.pop();
             if(stone) pq.push(stone);
         }
-        if(!pq.empty()) ans=pq.top();
-        return ans;
+        if(!pq.empty()) return pq.top();
+        return 0;
     }
 };
